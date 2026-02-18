eab72436de6c47731bc5962c59fd928f6057c5ef
BangTha <bangtha@Athas-MacBook-Pro.local>
2026-02-18 23:02:03 +0700

    fix products prerender

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 
 import { useCart } from "@/context/CartContext";
 import { Minus, Plus, Trash2 } from "lucide-react";
--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 
 import { useCart } from "@/context/CartContext";
 import { useRouter } from "next/navigation";
