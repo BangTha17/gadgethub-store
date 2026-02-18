85f65e3ab4a52ea0f943f7d2697a8aae25313161
BangTha <bangtha@Athas-MacBook-Pro.local>
2026-02-18 23:42:06 +0700

    fix products prerender

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,18 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const getSupabase = () => {
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-  if (!url || !anonKey) {
-    throw new Error("Supabase URL and ANON KEY must be set");
-  }
-
-  return createClient(url, anonKey, {
-    auth: {
-      persistSession: true,
-      detectSessionInUrl: true,
-      autoRefreshToken: true,
-    },
-  });
+  return createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
 };
+
